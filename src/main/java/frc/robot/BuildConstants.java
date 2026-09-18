27b09407312e64f2e0468f1e5b8063f94cf7174c
27ravez <zachary@jaraven.com>
2026-09-18 19:21:12 -0400

    Misc changes

---
+++ src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,19 @@
+package frc.robot;
+
+/**
+ * Automatically generated file containing build version information.
+ */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "ZipDriveTemplate";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 14;
+  public static final String GIT_SHA = "0b61aeaf886fdf7ac137db705f737b7cd3233e2d";
+  public static final String GIT_DATE = "2026-09-18 19:04:06 EDT";
+  public static final String GIT_BRANCH = "yagsl";
+  public static final String BUILD_DATE = "2026-09-18 19:13:36 EDT";
+  public static final long BUILD_UNIX_TIME = 1789773216943L;
+  public static final int DIRTY = 1;
+
+  private BuildConstants(){}
+}
--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -5,10 +5,18 @@
 package frc.robot;
 
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import edu.wpi.first.math.Matrix;
+import edu.wpi.first.math.VecBuilder;
+import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.numbers.N1;
+import edu.wpi.first.math.numbers.N3;
 import edu.wpi.first.math.trajectory.TrapezoidProfile;
-import edu.wpi.first.math.util.Units;
+import edu.wpi.first.units.Units;
+import edu.wpi.first.units.measure.Angle;
+import edu.wpi.first.units.measure.Distance;
 
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
@@ -35,6 +43,75 @@ public final class Constants {
             kMaxAngularSpeedRadiansPerSecond, kMaxAngularAccelerationRadiansPerSecondSquared);
   }
 
+  public static class VisionConstants {
+    // Offsets, not measured so a transform of zero
+    // Positive x is forward, positive y is left, positive z is up
+    // Our official configuration has, front left is camera 1, and back left is camera 2,
+    // back right is camera 3, front right is camera 4
+    public static final Transform3d kRobotToCam1 =
+        new Transform3d(
+            Distance.ofRelativeUnits(11.5, Units.Inches),
+            Distance.ofRelativeUnits(11.5, Units.Inches),
+            Distance.ofRelativeUnits(9.5, Units.Inches),
+            new Rotation3d(
+                Angle.ofRelativeUnits(0, Units.Degrees),
+                Angle.ofRelativeUnits(15, Units.Degrees),
+                Angle.ofRelativeUnits(45, Units.Degrees)));
+
+    public static final Transform3d kRobotToCam2 =
+        new Transform3d(
+            Distance.ofRelativeUnits(-11.5, Units.Inches),
+            Distance.ofRelativeUnits(11.5, Units.Inches),
+            Distance.ofRelativeUnits(9.5, Units.Inches),
+            new Rotation3d(
+                Angle.ofRelativeUnits(0, Units.Degrees),
+                Angle.ofRelativeUnits(15, Units.Degrees),
+                Angle.ofRelativeUnits(135, Units.Degrees)));
+
+    public static final Transform3d kRobotToCam3 =
+        new Transform3d(
+            Distance.ofRelativeUnits(-11.5, Units.Inches),
+            Distance.ofRelativeUnits(-11.5, Units.Inches),
+            Distance.ofRelativeUnits(9.5, Units.Inches),
+            new Rotation3d(
+                Angle.ofRelativeUnits(0, Units.Degrees),
+                Angle.ofRelativeUnits(15, Units.Degrees),
+                Angle.ofRelativeUnits(225, Units.Degrees)));
+
+    public static final Transform3d kRobotToCam4 =
+        new Transform3d(
+            Distance.ofRelativeUnits(11.5, Units.Inches),
+            Distance.ofRelativeUnits(-11.5, Units.Inches),
+            Distance.ofRelativeUnits(9.5, Units.Inches),
+            new Rotation3d(
+                Angle.ofRelativeUnits(0, Units.Degrees),
+                Angle.ofRelativeUnits(15, Units.Degrees),
+                Angle.ofRelativeUnits(315, Units.Degrees)));
+
+    public static final int kNotStaleTime = 3;
+    public static final int kOdometryUpdateFrequency = 10;
+
+    // Note that this is used to divide, making vision trusted more
+    public static final double kVisionOdometryStandardDevScalar = 5;
+
+    // The standard deviations of our vision estimated poses, which affect correction rate
+    // (Fake values. Experiment and determine estimation noise on an actual robot.)
+    public static final Matrix<N3, N1> kSingleTagStdDevs = VecBuilder.fill(4, 4, 8);
+    public static final Matrix<N3, N1> kMultiTagStdDevs = VecBuilder.fill(0.5, 0.5, 1);
+
+    public static final Translation2d kBlueAllianceHub =
+        new Translation2d(
+            Distance.ofRelativeUnits(182.105, Units.Inches),
+            Distance.ofRelativeUnits(158.84, Units.Inches));
+
+    public static final Translation2d kRedAllianceHub =
+        new Translation2d(
+            Distance.ofRelativeUnits(469.115, Units.Inches),
+            Distance.ofRelativeUnits(158.84, Units.Inches));
+
+    public static final double kAutoAimP = 1.2;
+  }
+
   // Constants specifically for Driving & Operation
   public static class DriveConstants {
     // Controller Ports ---
@@ -49,12 +126,6 @@ public final class Constants {
     public static final int kDriveRotate = 4;
     public static final double deadzoneDriver = 0.12;
 
-    // Speed Mode Strings
-    // Moved from DrivetrainSubsystem
-    public static final String low = "speed1";
-    public static final String medium = "speed2";
-    public static final String high = "speed3";
-
     public enum joysticks {
       DRIVER,
       OPERATOR
@@ -63,28 +134,19 @@ public final class Constants {
     // Driving Parameters - Note that these are not the maximum capable speeds of
     // the robot, rather the allowed maximum speeds
 
-    public static final double kMaxSpeedMetersPerSecond = 6.8;
-    public static final double kMaxAngularSpeed = 1.25 * 2 * Math.PI; // radians per second
-
-    // Drive Mode Speeds
-    // High
-    public static final double kSpeedHighDrive = 6.0;
-    public static final double kSpeedHighTurn = kMaxAngularSpeed;
-
-    // Medium is Default Speeds
-
-    // Slow
-    public static final double kSpeedSlowDrive = 2.1;
-    public static final double kSpeedSlowTurn = 1.8;
+    public static final double kMaxSpeedMetersPerSecond = 15;
+    public static final double kMaxAngularSpeed = 2.5 * 2 * Math.PI; // radians per second
 
     public static final double kDirectionSlewRate = 4; // radians per second
     public static final double kMagnitudeSlewRate = 2; // percent per second (1 = 100%)
     public static final double kRotationalSlewRate = 5; // percent per second (1 = 100%)
 
     // Chassis configuration
-    public static final double kTrackWidth = Units.inchesToMeters(26.5);
+    public static final double kTrackWidth =
+        Distance.ofRelativeUnits(26.5, Units.Inches).in(Units.Meters);
     // Distance between centers of right and left wheels on robot
-    public static final double kWheelBase = Units.inchesToMeters(26.5);
+    public static final double kWheelBase =
+        Distance.ofRelativeUnits(26.5, Units.Inches).in(Units.Meters);
     // Distance between front and back wheels on robot
     public static final SwerveDriveKinematics kDriveKinematics =
         new SwerveDriveKinematics(
@@ -97,26 +159,44 @@ public final class Constants {
   // Constants specifically for the physical robot
   public static final class RobotConstants {
     // SPARK MAX CAN IDs
-    public static final int kFrontRightDrivingCanId = 2;
-    public static final int kFrontRightTurningCanId = 1;
+    public static final int kFrontRightDrivingCanId = 3;
+    public static final int kFrontRightTurningCanId = 2;
 
-    public static final int kFrontLeftDrivingCanId = 4;
-    public static final int kFrontLeftTurningCanId = 3;
+    public static final int kFrontLeftDrivingCanId = 5;
+    public static final int kFrontLeftTurningCanId = 4;
 
-    public static final int kRearRightDrivingCanId = 6;
-    public static final int kRearRightTurningCanId = 5;
+    public static final int kRearRightDrivingCanId = 7;
+    public static final int kRearRightTurningCanId = 6;
 
-    public static final int kRearLeftDrivingCanId = 8;
-    public static final int kRearLeftTurningCanId = 7;
+    public static final int kRearLeftDrivingCanId = 9;
+    public static final int kRearLeftTurningCanId = 8;
+
+    public static final int kIntakeLiftCanId = 12;
+    public static final int kIntakeWheelCanId = 13;
+    public static final int kShooterRightCanId = 14;
+    public static final int kShooterLeftCanId = 15;
+    public static final int kFeederWheelCanId = 17;
+    public static final int kFeederConveyorCanId = 16;
+    public static final int kClimberCanId = 21;
 
     // Used to declare Navx as upside down
-    public static final boolean kGyroReversed = true;
+    public static final boolean kGyroReversed = false;
 
     // Angular offsets of the modules relative to the chassis in radians
     public static final double kFrontLeftChassisAngularOffset = -Math.PI / 2;
     public static final double kFrontRightChassisAngularOffset = 0;
     public static final double kBackLeftChassisAngularOffset = Math.PI;
     public static final double kBackRightChassisAngularOffset = Math.PI / 2;
+
+    public static final double kIntakeLiftDownSpeed = -0.1;
+    public static final double kIntakeLiftUpSpeed = 0.1;
+    public static final double kIntakeLiftOutPosition = 0.12; // measured 0.112
+    public static final double kIntakeLiftInPosition = 0.39; // measured 0.395
+
+    public static final double kIntakeWheelSpeed = -1;
+    public static final double kFeederWheelSpeed = -0.4;
+    public static final double kFeederConveyorSpeed = -0.7;
+    public static final double kClimberSpeed = 0.3;
   }
 
   // Constants specifically for Swerve Module
